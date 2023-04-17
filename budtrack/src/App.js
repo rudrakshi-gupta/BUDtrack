4d941856d79a3b4aad53727ad24f22ebe8dc4d18
red_hoodie <rudrakshigupta305@gmail.com>
2023-04-17 12:20:56 +0530

    BUDtrack app add

---
+++ budtrack/src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <>
+    <div className="BUDtrack">
+      <header className="App-header">
+        <div className="px-4 mx-auto max-w-7xl sm:px-6 lg:px-8">
+          <h2 className="text-2xl font-bold leading-tight text-gray-900">
+            Map
+          </h2>
+        </div>
+      </header>
+      <main>
+        <div className="mx-auto">
+          <div className="px-4 py-8 space-y-8"></div>
+        </div>
+      </main>
+    </div>
+    </>
+  );
+}
+
+export default App;
